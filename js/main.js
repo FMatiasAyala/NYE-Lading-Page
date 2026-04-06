71f7d21bea73873f43b59bc6dbe42b600c95bf1a
Matt Ayala <mattayala_15@outlook.com>
2026-04-06 01:34:52 -0300

    feat: cambios solicitados

--- js/main.js
+++ js/main.js
@@ -51,3 +51,34 @@ window.addEventListener("load", function () {
 function toggleMenu() {
   document.querySelector(".navbar-menu").classList.toggle("active");
 }
+
+// INICIALIZACIÓN SWIPER (Casos de Éxito)
+document.addEventListener("DOMContentLoaded", () => {
+  const casesSwiper = new Swiper('.cases-swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      800: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1100: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      }
+    }
+  });
+});
